8e60fe83bd9f5731abb71d58298f5a402d711121
LCTheo <34814044+LCTheo@users.noreply.github.com>
2020-04-14 16:59:26 +0200

    front fix

--- front/APIrequest.js
+++ front/APIrequest.js
@@ -2,27 +2,39 @@
 
 function Download(){
     var request = new XMLHttpRequest();
-
+    let user = localStorage.getItem('username');
+    let token = localStorage.getItem('token');
 // Open a new connection, using the GET request on the URL endpoint
-    request.open('GET', 'http://localhost:5000/rsc/test3?token=\'test\'', true);
+    request.open('GET', 'http://localhost:5000/rsc/'+user+'?token='+token, true);
 
     request.onload = function() {
         var data = JSON.parse(this.response);
-        document.getElementById("container_form").innerHTML += data;
-        //alert(data);
-    }
+        alert()
+        i = 0
+        while(data["response"].length > i){
+            document.getElementById("container_form").innerHTML += "<br><a href=\"http://192.168.137.12:5000/rsc/"+user+"/"+data["response"][i]+"?token="+token+"\" download>"+data["response"][i]+"</a>"
+            i++
+        }
 
+        var blob = new Blob([request.response], { type: 'file' });
+        var link = document.createElement('a');
+        link.href = window.URL.createObjectURL(blob);
+        link.download = this.response;
+
+        document.body.appendChild(link);
+      }
 // Send request
     request.send()
 }
 
 function Upload(){
-
+      let user = localStorage.getItem('username');
+      let token = localStorage.getItem('token');
     var fd = new FormData();
     fd.append('file', document.forms['fileinfo']['myfile'].files[0] /*, optional filename */)
 
     var req = $.ajax({
-        url: 'http://localhost:5000/rsc/test3?token=\'test\'',
+        url: 'http://localhost:5000/rsc/'+user+'?token='+token,
         method: 'POST',
         data: fd,
         processData: false, // don't let jquery process the data
@@ -65,4 +77,34 @@ function Inscription(){
     }, function(xhr) {
         console.error('failed to fetch xhr', xhr)
     })
+}
+
+function Connection(){
+
+    let data = $("form").serialize()
+
+    var req = $.ajax({
+        url: 'http://localhost:5001/users/',
+        type:'GET',
+        data: data,
+        success: [function(){
+            alert('User Connected');
+        }]
+    });
+
+    req.then(function(response) {
+        regex = /b'(?<token>.+)'/gm;
+        while ((m = regex.exec(response["response"].toString())) !== null) {
+    // This is necessary to avoid infinite loops with zero-width matches
+    if (m.index === regex.lastIndex) {
+        regex.lastIndex++;
+    }
+    newtoken = m[1]
+}
+        localStorage.setItem('token', newtoken);
+        localStorage.setItem('username', document.getElementById('id').value);
+    }, function(xhr) {
+        console.error('failed to fetch xhr', xhr)
+    })
+
 }
